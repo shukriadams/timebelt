351b24d355109051ecd05c7bceb0348cb06d5335
shukri adams <shukri.adams@gmail.com>
2018-08-17 11:05:42 +0000

    added toShortDate function

--- index.js
+++ index.js
@@ -3,6 +3,18 @@ let hoursDifference = function hoursDifference(before, after){
     return Math.floor(diff / (1000 * 60 * 60));
 }
 
+let toShortDate = function (date, format = 'y/m/d'){
+    if (typeof date === 'number' || typeof date === 'string')
+        date = new Date(date);
+
+    format = format.replace('y', date.getFullYear());
+    format = format.replace('m', date.getMonth() + 1);
+    format = format.replace('d', date.getDate() + 1);
+
+    return format;
+}
+
 module.exports = {
-    hoursDifference
+    hoursDifference,
+    toShortDate
 }
